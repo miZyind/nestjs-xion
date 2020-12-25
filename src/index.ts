9b593a2b4b0f0d7aa0232d0eadc4ebc76a34b246
miZyind <mizyind@gmail.com>
2020-12-25 13:57:45 +0700

    refactor: project structure

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './decorator';
-export * from './guarder';
-export * from './interceptor';
-export * from './model';
-export * from './standardizer';
---
+++ src/swagger.ts
@@ -0,0 +1,33 @@
+import type { SwaggerCustomOptions } from '@nestjs/swagger';
+
+export function customOptions(appName: string): SwaggerCustomOptions {
+  return {
+    swaggerOptions: {
+      defaultModelExpandDepth: 10,
+      defaultModelsExpandDepth: -1,
+    },
+    customCss: `
+      .swagger-ui .topbar { display: none }
+      .swagger-ui .information-container .info { margin: 20px 0 }
+      .swagger-ui .scheme-container {
+        padding: unset;
+        background: unset;
+        box-shadow: unset;
+        margin: -60px 0 0 0;
+        padding-bottom: 30px;
+      }
+      .swagger-ui .download-contents { display: none }
+      .swagger-ui .copy-to-clipboard {
+        bottom: 5px;
+        right: 10px;
+        width: 20px;
+        height: 20px;
+      }
+      .swagger-ui .copy-to-clipboard button {
+        padding-left: 18px;
+        height: 18px;
+      }
+    `,
+    customSiteTitle: `${appName} API`,
+  };
+}
