c822b6cf967b994e92bc0c5bc376102597a4cb06
miZyind <mizyind@gmail.com>
2021-01-27 14:07:28 +0700

    feat: add CognitoUserJWTPayload model

--- src/model.ts
+++ src/model.ts
@@ -11,3 +11,27 @@ export interface StandardList<T> {
   page?: number;
   pageCount?: number;
 }
+
+// https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-attributes.html
+export interface CognitoUserJWTPayload {
+  sub: string; // UUID
+  name: string;
+  given_name: string;
+  family_name: string;
+  middle_name: string;
+  nickname: string;
+  preferred_username: string;
+  profile: string;
+  picture: string;
+  website: string;
+  email: string;
+  email_verified: boolean;
+  gender: 'female' | 'male';
+  birthdate: string;
+  zoneinfo: string;
+  locale: string;
+  phone_number: string;
+  phone_number_verified: boolean;
+  // https://openid.net/specs/openid-connect-core-1_0.html#AddressClaim
+  address: { formatted: string };
+}
