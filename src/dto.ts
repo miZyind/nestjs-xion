867fb7de71d197d89057e71b95bd9ffee57ebf02
miZyind <mizyind@gmail.com>
2022-02-21 15:48:04 +0700

    feat: add PaginationQueryDTO

--- src/dto.ts
+++ src/dto.ts
@@ -2,12 +2,18 @@ import { Type } from 'class-transformer';
 import {
   IsInt,
   IsNotEmpty,
+  IsOptional,
   IsPositive,
   IsString,
   IsUUID,
 } from 'class-validator';
 
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+import {
+  DEFAULT_PAGINATION_LIMIT,
+  DEFAULT_PAGINATION_PAGE,
+} from './interceptor';
 
 export class UUIDParamDTO {
   @IsUUID()
@@ -28,4 +34,32 @@ export class IntIDParamDTO {
   @IsInt()
   @ApiProperty({ type: 'integer', minimum: 1 })
   readonly id!: number;
+}
+
+export class PaginationQueryDTO {
+  @IsOptional()
+  @IsPositive()
+  @IsInt()
+  @Type(() => Number)
+  @ApiPropertyOptional({
+    type: 'integer',
+    default: DEFAULT_PAGINATION_LIMIT,
+    minimum: 1,
+    description:
+      'Limit amount of resources<h5><a target="_blank" href="https://github.com/nestjsx/crud/wiki/Requests#limit">Documentation</a></h5><i>Minimum value: 1</i>',
+  })
+  readonly limit?: number;
+
+  @IsOptional()
+  @IsPositive()
+  @IsInt()
+  @Type(() => Number)
+  @ApiPropertyOptional({
+    type: 'integer',
+    default: DEFAULT_PAGINATION_PAGE,
+    minimum: DEFAULT_PAGINATION_PAGE,
+    description:
+      'Page portion of resources<h5><a target="_blank" href="https://github.com/nestjsx/crud/wiki/Requests#page">Documentation</a></h5><i>Minimum value: 1</i>',
+  })
+  readonly page?: number;
 }
