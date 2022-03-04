bfc7d1ae2ce700f0af4fb9e68286f1941f76f65f
miZyind <mizyind@gmail.com>
2022-03-04 17:26:33 +0700

    feat: add getValuable guarder

--- src/guarder.ts
+++ src/guarder.ts
@@ -1,3 +1,12 @@
-export function hasValue<T>(obj: T | null | undefined | void): obj is T {
-  return typeof obj !== 'undefined' && obj !== null;
+export function hasValue<T>(v: T | null | undefined | void): v is T {
+  return typeof v !== 'undefined' && v !== null;
+}
+
+export function getValuable<
+  T,
+  V = { [K in keyof T as T[K] extends null | undefined ? never : K]: T[K] },
+>(entity: T): V {
+  return Object.fromEntries(
+    Object.entries(entity).filter(([, v]) => hasValue(v)),
+  ) as V;
 }
